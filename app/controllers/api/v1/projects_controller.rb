476ceb27b0741da8ce8b1787447978abcde30b80
cavu757 <jose.pehovazdiez@gmail.com>
2020-01-04 15:01:03 -0500

    Update project controller index method

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,2 +1,26 @@
 class Api::V1::ProjectsController < ApplicationController
+
+  def index
+    @projects = Project.all
+    render json: @projects
+  end
+
+  def create
+    
+  end
+
+  def show
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description, :deadline)
+  end
+
 end
