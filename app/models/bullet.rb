d1ed025c8c39e3ca1f63b96721ab5b03e8d013f6
cavu757 <jose.pehovazdiez@gmail.com>
2020-01-04 14:09:12 -0500

    added seeds and updated bullet model
    added seed file and updated bullet model to require project_id

--- app/models/bullet.rb
+++ app/models/bullet.rb
@@ -1,7 +1,7 @@
 class Bullet < ApplicationRecord
   belongs_to :project
-  validates :content, :date, presence: true
+  validates :project_id, :content, :date, presence: true
   validates_inclusion_of :category, :in => ['task','event','note']
 
-  
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+project = Project.create(name: "reactjs project 2", deadline: "2020-06-04")
+bullet_one = Bullet.create(project_id: 1, content: "Reach out to instructor", date: DateTime.now, category: "task")
+bullet_two = Bullet.create(project_id: 1, content: "Fill out bullet journal", date: DateTime.now, category: "task")
+bullet_three = Bullet.create(project_id: 1, content: "Add one event", date: DateTime.now, category: "event")
+bullet_four = Bullet.create(project_id: 1, content: "Add a second event", date: DateTime.now, category: "event")
+bullet_five = Bullet.create(project_id: 1, content: "One more task", date: DateTime.now, category: "task")
+bullet_six = Bullet.create(project_id: 1, content: "Final bullet is a note", date: DateTime.now, category: "note")
