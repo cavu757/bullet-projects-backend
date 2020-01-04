f44dba9ed3c7c14e6ed5dd4f90e6f4a21897de83
cavu757 <jose.pehovazdiez@gmail.com>
2020-01-04 14:50:17 -0500

    add seeds and project method
    add days_left on project method

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,14 @@
 class Project < ApplicationRecord
   has_many :bullets
   validates :name, :deadline, presence: true
+
+  def days_left
+    days_left = ((self.deadline - DateTime.now)/86400).round
+    if days_left <= 0
+      return "OVERDUE"
+    else
+      return days_left
+    end
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-project = Project.create(name: "reactjs project 2", deadline: "2020-06-04")
-bullet_one = Bullet.create(project_id: 1, content: "Reach out to instructor", date: DateTime.now, category: "task")
+project_one = Project.create(name: "ReactJS project", deadline: "2020-06-04")
+bullet_one = Bullet.create(project_id: 1, content: "Finish backend", date: DateTime.now, category: "task")
 bullet_two = Bullet.create(project_id: 1, content: "Fill out bullet journal", date: DateTime.now, category: "task")
 bullet_three = Bullet.create(project_id: 1, content: "Add one event", date: DateTime.now, category: "event")
 bullet_four = Bullet.create(project_id: 1, content: "Add a second event", date: DateTime.now, category: "event")
 bullet_five = Bullet.create(project_id: 1, content: "One more task", date: DateTime.now, category: "task")
 bullet_six = Bullet.create(project_id: 1, content: "Final bullet is a note", date: DateTime.now, category: "note")
+
+project_two = Project.create(name: "Mady's Birthday", deadline: "2020-02-08")
+
+project_three = Project.create(name: "Overdue project", deadline: "2019-12-04")
