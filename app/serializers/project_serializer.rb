0754838b86258c0eb2fbce00e4bb04ca62912a65
cavu757 <jose.pehovazdiez@gmail.com>
2020-01-05 15:29:26 -0500

    updated serializer to send days left in project

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,4 +1,4 @@
 class ProjectSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :deadline
+  attributes :id, :name, :description, :deadline, :days_left
   has_many :bullets
 end
