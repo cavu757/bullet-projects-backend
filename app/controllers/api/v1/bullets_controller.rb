5125ed63d5fc9e1bd0b5b9cdd07074fc781d532e
cavu757 <jose.pehovazdiez@gmail.com>
2020-01-04 20:32:12 -0500

    set up bullet controllers
    add create, show, index, destroy

--- app/controllers/api/v1/bullets_controller.rb
+++ app/controllers/api/v1/bullets_controller.rb
@@ -1,5 +1,39 @@
 class Api::V1::BulletsController < ApplicationController
 
-  
+  before_action :set_project
+
+  def index
+    @bullets = @project.bullets
+    render json: @bullets
+  end
+
+  def create
+    @bullet = @project.bullets.new(bullet_params)
+    if @bullet.save
+      render json: @bullet
+    else
+      render json: {error: 'Not enough info to create bullet'}
+    end
+  end
+
+  def show
+    @bullet = Bullet.find(params[:id])
+    render json: @bullet
+  end
+
+  def destroy
+    @bullet = Bullet.find(params[:id])
+    @bullet.destroy
+  end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:project_id])
+  end
+
+  def bullet_params
+    params.require(:bullet).permit(:project_id, :content, :category, :priority, :date)
+  end
 
 end
